0adcbb76f9258150e77fe461ecfe1bcf19c61bb3
John Cordeiro <john.dalton@ilhasoft.com.br>
2017-05-18 19:22:06 -0300

    Add base permission dialog

---
+++ fcm-channel/src/main/java/io/rapidpro/sdk/permission/PermissionDialog.java
@@ -0,0 +1,19 @@
+package io.rapidpro.sdk.permission;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by John Cordeiro on 5/18/17.
+ * Copyright © 2017 rapidpro-android-sdk, Inc. All rights reserved.
+ */
+
+public class PermissionDialog extends Dialog {
+
+    public PermissionDialog(@NonNull Context context) {
+        super(context);
+    }
+
+
+}
