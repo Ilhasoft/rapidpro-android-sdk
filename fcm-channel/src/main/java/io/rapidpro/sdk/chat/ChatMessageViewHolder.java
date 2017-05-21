d547fa94646da498911cd434e3fbd9a1b79259d7
John Cordeiro <john.dalton@ilhasoft.com.br>
2017-05-21 11:47:58 -0300

    Refactor chat message view configuration on view holder

--- fcm-channel/src/main/java/io/rapidpro/sdk/chat/ChatMessageViewHolder.java
+++ fcm-channel/src/main/java/io/rapidpro/sdk/chat/ChatMessageViewHolder.java
@@ -5,21 +5,20 @@ import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.DrawableRes;
 import android.support.v7.widget.RecyclerView;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.text.DateFormat;
 
 import io.rapidpro.sdk.R;
 import io.rapidpro.sdk.core.models.Message;
-import io.rapidpro.sdk.util.PicassoImageGetter;
 
 /**
  * Created by john-mac on 4/11/16.
@@ -38,82 +37,69 @@ class ChatMessageViewHolder extends RecyclerView.ViewHolder {
 
     private final DateFormat hourFormatter;
     private final Context context;
-    private final PicassoImageGetter imageGetter;
 
-    public ChatMessageViewHolder(Context context, ViewGroup parent, @DrawableRes int iconRes) {
+    private final int leftMarginIncoming;
+    private final int leftMarginOutgoing;
+    private final int bottomMarginIncoming;
+    private final int bottomMarginOutgoing;
+
+    ChatMessageViewHolder(Context context, ViewGroup parent, @DrawableRes int iconRes) {
         super(LayoutInflater.from(context).inflate(R.layout.fcm_client_item_chat_message, parent, false));
         this.context = context;
         this.hourFormatter = DateFormat.getTimeInstance(DateFormat.SHORT);
         this.parent = (ViewGroup) itemView.findViewById(R.id.bubble);
 
         this.message = (TextView) itemView.findViewById(R.id.chatMessage);
-        this.message.setMovementMethod(LinkMovementMethod.getInstance());
         this.date = (TextView) itemView.findViewById(R.id.chatMessageDate);
 
         this.icon = (ImageView) itemView.findViewById(R.id.icon);
         this.icon.setImageResource(iconRes);
 
         this.itemView.setOnLongClickListener(onLongClickListener);
-        this.imageGetter = new PicassoImageGetter(message, R.drawable.fcm_client_ic_error_outline_grey);
+
+        this.leftMarginIncoming = getDpDimen(10);
+        this.leftMarginOutgoing = getDpDimen(45);
+        this.bottomMarginIncoming = getDpDimen(3);
+        this.bottomMarginOutgoing = getDpDimen(15);
     }
 
-    public void bindView(Message chatMessage) {
+    void bindView(Message chatMessage) {
         this.chatMessage = chatMessage;
-        message.setText(Html.fromHtml(chatMessage.getText(), imageGetter, null));
+        message.setText(chatMessage.getText());
+        Linkify.addLinks(message, Linkify.ALL);
         date.setText(hourFormatter.format(chatMessage.getCreatedOn()));
 
         bindContainer(chatMessage);
     }
 
     private void bindContainer(Message chatMessage) {
-        boolean userAuthor = isUserAuthor(chatMessage);
-        icon.setVisibility(userAuthor ? View.GONE : View.VISIBLE);
+        boolean incoming = isIncoming(chatMessage);
+        icon.setVisibility(incoming ? View.GONE : View.VISIBLE);
 
-        int smallSpace = getDpDimen(10);
-        int largeSpace = getDpDimen(40);
-
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT
-                , ViewGroup.LayoutParams.WRAP_CONTENT);
-        addRule(userAuthor, params);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            params.setMarginStart(userAuthor ? largeSpace : smallSpace);
-        } else {
-            params.leftMargin = userAuthor ? largeSpace : smallSpace;
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            params.setMarginEnd(userAuthor ? smallSpace : largeSpace);
-        } else {
-            params.rightMargin = userAuthor ? smallSpace : largeSpace;
-        }
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) parent.getLayoutParams();
+        setupBubblePosition(incoming, params);
         parent.setLayoutParams(params);
 
-        int drawable = userAuthor ? R.drawable.fcm_client_bubble_me : R.drawable.fcm_client_bubble_other;
+        int drawable = incoming ? R.drawable.fcm_client_bubble_me : R.drawable.fcm_client_bubble_other;
         parent.setBackgroundResource(drawable);
 
-        int textColor = userAuthor ? Color.BLACK : Color.WHITE;
+        int textColor = incoming ? Color.BLACK : Color.WHITE;
         message.setTextColor(textColor);
     }
 
-    private boolean isUserAuthor(Message chatMessage) {
-        return chatMessage.getDirection().equals(Message.DIRECTION_INCOMING);
+    private void setupBubblePosition(boolean incoming, FrameLayout.LayoutParams params) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            params.gravity = incoming ? Gravity.END : Gravity.START;
+            params.setMarginStart(incoming ? leftMarginIncoming : leftMarginOutgoing);
+        } else {
+            params.gravity = incoming ? Gravity.RIGHT : Gravity.LEFT;
+            params.leftMargin = incoming ? leftMarginIncoming : leftMarginOutgoing;
+        }
+        params.bottomMargin = incoming ? bottomMarginIncoming : bottomMarginOutgoing;
     }
 
-    private void addRule(boolean userAuthor, RelativeLayout.LayoutParams params) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            if (!userAuthor) {
-                params.addRule(RelativeLayout.END_OF, R.id.icon);
-            } else {
-                params.addRule(RelativeLayout.ALIGN_PARENT_END);
-            }
-        } else {
-            if (!userAuthor) {
-                params.addRule(RelativeLayout.RIGHT_OF, R.id.icon);
-            } else {
-                params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-            }
-        }
-
+    private boolean isIncoming(Message chatMessage) {
+        return chatMessage.getDirection().equals(Message.DIRECTION_INCOMING);
     }
 
     private int getDpDimen(int value) {
@@ -131,11 +117,11 @@ class ChatMessageViewHolder extends RecyclerView.ViewHolder {
         }
     };
 
-    public void setOnChatMessageSelectedListener(OnChatMessageSelectedListener onChatMessageSelectedListener) {
+    void setOnChatMessageSelectedListener(OnChatMessageSelectedListener onChatMessageSelectedListener) {
         this.onChatMessageSelectedListener = onChatMessageSelectedListener;
     }
 
-    public interface OnChatMessageSelectedListener {
+    interface OnChatMessageSelectedListener {
         void onChatMessageSelected(Message chatMessage);
     }
 }
