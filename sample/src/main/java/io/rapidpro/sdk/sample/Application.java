2da9bd4f2177b1d9c0f58d2f19e3b83257e45ded
John Cordeiro <john.dalton@ilhasoft.com.br>
2017-05-22 10:04:50 -0300

    Add missing ui configuration on sample project

--- sample/src/main/java/io/rapidpro/sdk/sample/Application.java
+++ sample/src/main/java/io/rapidpro/sdk/sample/Application.java
@@ -1,5 +1,8 @@
 package io.rapidpro.sdk.sample;
 
+import android.support.annotation.ColorRes;
+import android.support.v4.content.res.ResourcesCompat;
+
 import io.rapidpro.sdk.FcmClient;
 import io.rapidpro.sdk.UiConfiguration;
 import io.rapidpro.sdk.sample.services.PushRegistrationService;
@@ -24,6 +27,12 @@ public class Application extends android.app.Application {
                         .setPermissionMessage("Please give me permission to open floating chat!")
                         .setTheme(R.style.AppTheme_Blue)
                         .setIconResource(R.mipmap.ic_launcher)
+                        .setIconFloatingChat(R.mipmap.ic_launcher)
+                        .setTitleColor(getColorCompat(android.R.color.white))
                         .setTitleString("RapidPRO Sample SDK")));
+    }
+
+    private int getColorCompat(@ColorRes int colorRes) {
+        return ResourcesCompat.getColor(getResources(), colorRes, getTheme());
     }
 }
